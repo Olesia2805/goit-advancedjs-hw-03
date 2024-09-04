9426a138db97e50729bbfd9b30435359533e69a1
Olesia2805 <vysotina2002@gmail.com>
2024-09-04 23:32:26 +0200

    settings

---
+++ src/js/render-functions.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export function createImageCard(image) {}
+
+export function renderImages(images, container) {}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+// main.js
+// import { fetchImages } from './js/pixabay-api.js';
+// import { renderImages, showError } from './js/render-functions.js';
 
+// const form = ;
+// const input = ;
+// const gallery = ;
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+});
