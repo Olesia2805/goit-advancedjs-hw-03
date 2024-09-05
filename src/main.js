144164aee4f3cbebc8b8faeb5ed4428bc4b6ffc7
Olesia2805 <vysotina2002@gmail.com>
2024-09-05 23:48:55 +0200

    form, HTTP, createCards, SimpleLightbox

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-// main.js
-// import { fetchImages } from './js/pixabay-api.js';
-// import { renderImages, showError } from './js/render-functions.js';
+import { fetchImages } from './js/pixabay-api.js';
+import { renderImages } from './js/render-functions.js';
 
-// const form = ;
-// const input = ;
-// const gallery = ;
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-seach');
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+  const searchQuery = input.value.trim();
+
+  gallery.innerHTML = '';
+
+  fetchImages(searchQuery)
+    .then(data => {
+      if (data.hits.length !== 0) {
+        renderImages(data.hits, gallery);
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+    })
+    .finally(() => {
+      input.value = '';
+    });
 });
