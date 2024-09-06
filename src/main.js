90a1a960088e91c70156934db9697615c945125f
Olesia2805 <vysotina2002@gmail.com>
2024-09-06 23:14:22 +0200

    loader + check for empty input

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
 import { fetchImages } from './js/pixabay-api.js';
 import { renderImages } from './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-seach');
 const gallery = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.js-loader');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const searchQuery = input.value.trim();
 
+  if (searchQuery === '') {
+    iziToast.error({
+      position: 'topLeft',
+      message:
+        'Sorry, the field must not contain only spaces. Please, try again!',
+    });
+
+    form.reset();
+
+    return;
+  }
+
   gallery.innerHTML = '';
 
+  loaderEl.classList.remove('is-hidden');
+
   fetchImages(searchQuery)
+    .finally(() => {
+      loaderEl.classList.add('is-hidden');
+      input.value = '';
+    })
     .then(data => {
       if (data.hits.length !== 0) {
         renderImages(data.hits, gallery);
@@ -19,8 +40,5 @@ form.addEventListener('submit', event => {
     })
     .catch(error => {
       console.error('Error fetching images:', error);
-    })
-    .finally(() => {
-      input.value = '';
     });
 });
